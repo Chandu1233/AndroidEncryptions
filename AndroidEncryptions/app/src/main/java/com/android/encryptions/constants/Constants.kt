c743a2097d7b212696c27e8fc4aa71124517d1f9
ChandraSekhar <gpcsekhar.it@gmail.com>
2020-06-13 21:33:22 +0530

    t

--- AndroidEncryptions/app/src/main/java/com/android/encryptions/constants/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.android.encryptions.constants
-
-open class Constants {
-
-    companion object {
-        public val AES_ENCRYPTION = "AES"
-        public val RSA_ENCRYPTION = "RSA"
-    }
-}
