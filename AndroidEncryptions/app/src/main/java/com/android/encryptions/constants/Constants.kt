b08f49c871cea6143e6f0098ba94541203ad124a
ChandraSekhar <gpcsekhar.it@gmail.com>
2020-06-13 19:11:20 +0530

    added project code

---
+++ AndroidEncryptions/app/src/main/java/com/android/encryptions/constants/Constants.kt
@@ -0,0 +1,9 @@
+package com.android.encryptions.constants
+
+open class Constants {
+
+    companion object {
+        public val AES_ENCRYPTION = "AES"
+        public val RSA_ENCRYPTION = "RSA"
+    }
+}
---
+++ AndroidEncryptions/app/src/main/java/com/android/encryptions/test.java
@@ -0,0 +1,23 @@
+package com.android.encryptions;
+
+import java.io.UnsupportedEncodingException;
+
+public class test {
+
+
+    public void aa(){
+
+
+        try {
+            String x = "aa";
+            x.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
